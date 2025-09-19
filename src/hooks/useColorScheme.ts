fc63d4c98bd634ab019597e80ff3ece7105772c3
alinakassym <alinakassym@gmail.com>
2025-09-19 18:46:25 +0500

    fix(theme mode): add window theme

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -1,18 +1,32 @@
 import { useEffect, useState } from 'react';
 
-type Scheme = 'light' | 'dark';
+declare global {
+  interface Window {
+    __APP_THEME?: Scheme;
+  }
+}
 
-export function useColorScheme(): Scheme {
-  const get = (): Scheme =>
-    window.matchMedia?.('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+type Scheme = "light" | "dark";
 
+export function useColorScheme(defaultScheme: Scheme = "light"): Scheme {
+  const get = (): Scheme => {
+    const qs = new URLSearchParams(window.location.search).get(
+      "theme",
+    ) as Scheme | null;
+    const global = window?.__APP_THEME as Scheme | undefined;
+    const ls = (localStorage.getItem("APP_THEME") as Scheme | null) || null;
+    const media = window.matchMedia?.("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+    return (qs || global || ls || media || defaultScheme) as Scheme;
+  };
   const [scheme, setScheme] = useState<Scheme>(get());
 
   useEffect(() => {
-    const mql = window.matchMedia?.('(prefers-color-scheme: dark)');
-    const onChange = () => setScheme(mql.matches ? 'dark' : 'light');
-    mql?.addEventListener?.('change', onChange);
-    return () => mql?.removeEventListener?.('change', onChange);
+    const mql = window.matchMedia?.("(prefers-color-scheme: dark)");
+    const onChange = () => setScheme(mql.matches ? "dark" : "light");
+    mql?.addEventListener?.("change", onChange);
+    return () => mql?.removeEventListener?.("change", onChange);
   }, []);
 
   return scheme;
