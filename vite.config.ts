91792de24e6733634b56779e26626aeac9129e04
alinakassym <alinakassym@gmail.com>
2025-09-20 17:17:35 +0500

    chore: vite config, add types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/sharity-web/',
+  base: "/sharity-web/",
   plugins: [react()],
-  resolve: { dedupe: ['react', 'react-dom'] },
-})
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
