dfde9ff7f2f88b28bd92a7cccc6eed024cc2268d
alinakassym <alinakassym@gmail.com>
2025-10-12 22:49:22 +0500

    style(LocationHeader): absolute position

--- src/components/LocationHeader.tsx
+++ src/components/LocationHeader.tsx
@@ -18,7 +18,7 @@ const LocationHeader: FC<LocationHeaderProps> = ({
   return (
     <header
       style={{
-        position: "fixed",
+        position: "absolute",
         top: 0,
         left: 0,
         right: 0,
