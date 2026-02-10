c1c71224420d40612be6e9bf3125cd4674c7db25
alinakassym <alinakassym@gmail.com>
2026-02-10 14:26:10 +0500

    feat(VuesaxIcon): add edit and trash

--- src/components/icons/VuesaxIcon.tsx
+++ src/components/icons/VuesaxIcon.tsx
@@ -638,6 +638,83 @@ const icons: Record<string, IconDef> = {
       </>
     ),
   },
+  "edit-2": {
+    viewBox: "0 0 24 24",
+    paths: ({ stroke, strokeWidth }) => (
+      <>
+        <path
+          d="M13.26 3.59997L5.04997 12.29C4.73997 12.62 4.43997 13.27 4.37997 13.72L4.00997 16.96C3.87997 18.13 4.71997 18.93 5.87997 18.73L9.09997 18.18C9.54997 18.1 10.18 17.77 10.49 17.43L18.7 8.73997C20.12 7.23997 20.76 5.52997 18.55 3.43997C16.35 1.36997 14.68 2.09997 13.26 3.59997Z"
+          fill="transparent"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M11.89 5.05005C12.32 7.81005 14.56 9.92005 17.34 10.2"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M3 22H21"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </>
+    ),
+  },
+  trash: {
+    viewBox: "0 0 24 24",
+    paths: ({ stroke, strokeWidth }) => (
+      <>
+        <path
+          d="M21 5.97998C17.67 5.64998 14.32 5.47998 10.98 5.47998C9 5.47998 7.02 5.57998 5.04 5.77998L3 5.97998"
+          fill="transparent"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M8.5 4.97L8.72 3.66C8.88 2.71 9 2 10.69 2H13.31C15 2 15.13 2.75 15.28 3.67L15.5 4.97"
+          fill="transparent"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M18.85 9.14001L18.2 19.21C18.09 20.78 18 22 15.21 22H8.79002C6.00002 22 5.91002 20.78 5.80002 19.21L5.15002 9.14001"
+          fill="transparent"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10.33 16.5H13.66"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M9.5 12.5H14.5"
+          stroke={stroke ?? "currentColor"}
+          strokeWidth={strokeWidth ?? "2"}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </>
+    ),
+  },
 };
 
 const VuesaxIcon: FC<VuesaxIconProps> = ({
